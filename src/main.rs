9219f1c6c0804b68928b934f1b96f8748bfce32f
Kivooeo <Kivooeo123@gmail.com>
2023-06-11 01:55:10 +0500

    change main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+use std::io::{stdin, stdout, Write};
+
 use eval::f;
 
 fn main() {
-    f!("(123.45*(678.90 / (-2.5+ 11.5)-(((80 -(19))) *33.25)) / 20) - (123.45*(678.90 / (-2.5+ 11.5)-(((80 -(19))) *33.25)) / 20) + (13 - 2)/ -(-11) ");
+    loop {
+        print!(">>> ");
+        stdout().flush();
+        let mut input = String::new();
+        stdin().read_line(&mut input);
+        input.replace("\n", "");
+        if input.contains("|") {
+            let split = input.split("|").into_iter().collect::<Vec<&str>>();
+            let vars = split[1]
+                .trim()
+                .split_ascii_whitespace()
+                .into_iter()
+                .map(|x| x.parse::<f64>().unwrap())
+                .collect::<Vec<f64>>();
+            let expression = split[0].trim();
+            let answer = eval::math::eval(&eval::math::fill(expression, &vars));
+            println!("answer is {answer}");
+        } else {
+            println!("answer is {}", f!(&input));
+        }
+        input.clear();
+    }
 }
