550c69465c50a0a86a69393ae2ae3c2e551c69b8
Kivooeo <Kivooeo123@gmail.com>
2023-06-11 02:13:07 +0500

    chaged main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,13 @@ fn main() {
                 .map(|x| x.parse::<f64>().unwrap())
                 .collect::<Vec<f64>>();
             let expression = split[0].trim();
-            let answer = eval::math::eval(&eval::math::fill(expression, &vars));
-            println!("answer is {answer}");
+            let answer = eval::math::eval(&eval::math::fill(expression, &vars)).to_string();
+            if input.chars().filter(|x| *x == '|').count() == 2 {
+                let round = split[2].trim().parse::<i32>().unwrap();
+                println!("{}", eval::f!(&answer; round));
+            } else {
+                println!("{answer}");
+            }
         } else {
             println!("answer is {}", f!(&input));
         }
